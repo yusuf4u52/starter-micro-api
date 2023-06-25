a8afba599fb5d981f14be779858054370acc069c
Yusuf Rampurawala <yusuf4u52@gmail.com>
2023-06-25 16:37:49 +0530

    getqr

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const qrcode = require('qrcode-terminal');
-
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const client = new Client({
   authStrategy: new LocalAuth()
 });
-client.on('qr', qr => {
-  qrcode.generate(qr, { small: true });
+
+client.on("qr", (qr) => {
+  console.log("qr");
+  fs.writeFileSync("./components/last.qr", qr);
 });
 
 client.on('ready', () => {
@@ -37,6 +37,48 @@ app.post('/sendmessage', async (req, res, next) => {
     next(error);
   }
 });
+
+app.get("/getqr", async (res) => {
+  client
+    .getState()
+    .then((data) => {
+      if (data) {
+        res.write("<html><body><h2>Already Authenticated</h2></body></html>");
+        res.end();
+      } else sendQr(res);
+    })
+    .catch(() => sendQr(res));
+});
+
+function sendQr(res) {
+  fs.readFile("components/last.qr", (err, last_qr) => {
+    if (!err && last_qr) {
+      var page = `
+                    <html>
+                        <body>
+                            <script type="module">
+                            </script>
+                            <div id="qrcode"></div>
+                            <script type="module">
+                                import QrCreator from "https://cdn.jsdelivr.net/npm/qr-creator/dist/qr-creator.es6.min.js";
+                                let container = document.getElementById("qrcode");
+                                QrCreator.render({
+                                    text: "${last_qr}",
+                                    radius: 0.5, // 0.0 to 0.5
+                                    ecLevel: "H", // L, M, Q, H
+                                    fill: "#536DFE", // foreground color
+                                    background: null, // color or null for transparent
+                                    size: 256, // in pixels
+                                }, container);
+                            </script>
+                        </body>
+                    </html>
+                `;
+      res.write(page);
+      res.end();
+    }
+  });
+}
 
 client.initialize();
 
