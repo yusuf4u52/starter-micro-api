80e7fe55a37599801476535ad875fd043cc7b1d6
Yusuf Rampurawala <yusuf4u52@gmail.com>
2023-06-25 12:14:56 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,44 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+const qrcode = require('qrcode-terminal');
+
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const client = new Client({
+  authStrategy: new LocalAuth()
+});
+client.on('qr', qr => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+  console.log('Client is ready!');
+});
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+app.post('/sendmessage', async (req, res, next) => {
+  try {
+    console.log(req.body); const { number, message } = req.body; // Get the body
+    const msg = await client.sendMessage(`${number}@c.us`, message); // Send the message
+    res.send({ msg }); // Send the response
+  } catch (error) {
+    next(error);
+  }
+});
+
+client.initialize();
+
+
+
